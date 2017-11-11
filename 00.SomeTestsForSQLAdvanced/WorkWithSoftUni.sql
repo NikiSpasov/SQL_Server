3d35837569331bbae6f6bd30071f5f4a9f4ff197
FromiMac <niki.spasow@gmail.com>
2017-11-11 04:43:28 +0200

    after Exam

---
+++ 00.SomeTestsForSQLAdvanced/WorkWithSoftUni.sql
@@ -0,0 +1,6 @@
+--SELECT COUNT(*) FROM Users
+--       WHERE UserName = '{username}' AND +
+--       PasswordHash = '{CalcSHA1(password)}'
+
+SELECT COUNT(*) FROM Employees 
+ WHERE FirstName = ''; PRINT N'Opa'; --';
